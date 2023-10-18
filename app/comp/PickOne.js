46f996de890d82bd3fe8a48ddf37040c009d1d1d
skyg000 <skyg00700@gmail.com>
2023-10-18 19:10:32 +0900

    adsa

--- app/comp/PickOne.js
+++ app/comp/PickOne.js
@@ -1,34 +1,44 @@
 "use client"
 import pickone from '../pages/pickone/pickone.module.scss'
 import React, { useEffect, useState } from 'react'
+import { usePathname, useRouter } from 'next/navigation';
 import axios from "axios";
 
 
 function PickOne() {
-    const [pickList, setPickList] = useState([]);
-    useEffect(() => {
-        const id = typeof window !== 'undefined' ? window.sessionStorage.getItem("id") :null
-        // myPick이 true : 내가 찜한 애
-        // myPick이 false : 나를 찜한 애
-        axios.get(`/api/matchlist?myPick=false&id=${id}`)
-            .then((res) => {
-                setPickList(res.data);
-            });
-    }, []);
+    const [info, setInfo] = useState({});
+    const id = usePathname().split('/')[3];
+    const router = useRouter();
+
+    const onMatchButtonClick = () => {
+        axios.put('/api/matchlist', {
+            id: id,
+            opntid: typeof window !== 'undefined' ? sessionStorage.getItem('id') : null,
+            y_status: 'yes'
+        })
+        .then(() => router.push(`/pages/contact/${id}`));
+    }
     
+    useEffect(() => {
+        axios.get(`/api/member/${id}`)
+        .then(res => {
+            setInfo(res.data)
+        });
+    }, [])
+
     return (
         <div className={pickone.home}>
 
             <div className={pickone.cardtitle}>이런 이용자 님은 어떠세요? </div>
             <div className={pickone.card} >
-                <img src="../imges/main.png" />
-                <div>이름 (나이)</div>
+                <img src="../../imges/main.png" />
+                <div>{info.id}</div>
                 <div>서울대학교 애견미용학과 중퇴</div>
-                <div>서울 작동구 거주</div>
-                <div>웹퍼블리셔 재직 2년차</div>
+                <div>{info.adderss} 거주</div>
+                <div>{info.job}</div>
                 <div>모험을 즐기는 사업가 ESTP</div>
-                <div>눈웃음이 매력적이라는 이야기를 들어요!</div>
-                <button className={pickone.match}>매칭할래요!</button>
+                <div>{info.self}</div>
+                <button onClick={onMatchButtonClick} className={pickone.match}>매칭할래요!</button>
                 <div className={pickone.warning}>매칭은 하루에 한 번만 가능하며, 취소가 불가합니다.</div>
             </div>
 
