46923aa3dfd9ca4dcffc935ba003a0f72714ba2f
hsk <skyg00700@gmail.com>
2023-11-27 16:16:49 +0900

    11-27 bard

--- app/api/gptapi/route.js
+++ app/api/gptapi/route.js
@@ -1,39 +1,42 @@
-import { ChatGPTAPI } from "chatgpt";
+import { BardAI } from "bardie";
 
-export async function GET(req) {
-  const { username } = Object.fromEntries(req.nextUrl.searchParams);
-  const { gender, date, calendartype, time } = JSON.parse(username);
+export async function GET(req, res) {
+  const bard = new BardAI();
+  try {
+    const { username } = Object.fromEntries(req.nextUrl.searchParams);
+    const { gender, date, calendartype, time } = JSON.parse(username);
 
-  const api = new ChatGPTAPI({
-    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-    completionParams: {
-      model: "gpt-3.5-turbo",
-      temperature: 0.5,
-      top_p: 0.8,
-    },
-  });
+    const firstQuestion = `${calendartype} ${date} ${time} 출생 ${gender}의 만세력을 바탕으로 운세를 알려줘.`;
+    const firstResponse = await bard.question({ ask: firstQuestion });
 
-  let response1 = await api.sendMessage(
-    `${calendartype} ${date} ${time} 출생 ${gender}의 만세력을 바탕한 천간, 지지를 이용해서 초년운,중년운,말년운,형제운,자식운,부부운,직업운을 조금 자세히 알려줘. 정확하지 않아도 되니까 그냥 알려줘.필요에 따라 양력을 한국식 음력으로 바꿔도 되는데 참고로 양력 1992년 5월 29일의 음력은 4월 27일이야.`
-  );
+    const secondQuestion = `${calendartype} ${date} ${time} 출생 ${gender}인   나의 오행기운을 오행기운: 금(金):n개 목(木):n개  수(水):n개  화(火):n개  토(土):n개 이런식으로 아무 대답도 설명도 하지말고 갯수만 적어줘. 0개인 경우도 적어줘.형식에 변화는 주지말고 저 오행단어는 그대로 써줘 `;
+    const secondResponse = await bard.question({ ask: secondQuestion });
 
-  // 첫 번째 응답을 기반으로 두 번째 질문을 합니다.
-  let response2 = await api.sendMessage(
-    "저 사주를 바탕으로 나의 오행 갯수를 화(火): 0개 수(水): 0개 목(木): 0개  금(金): 0개 토(土): 0개 이런식으로 띄어쓰기와 설명없이 그냥 갯수만 말해줘.",
-    {
-      parentMessageId: response1.id,
+    const thirdQuestion = `${calendartype} ${date} ${time} 출생 ${gender}인   나의 인연 오행기운을 오행기운: 금(金):n개 목(木):n개  수(水):n개  화(火):n개  토(土):n개 이런식으로 아무 대답도 설명도 하지말고 갯수만 적어줘. 0개인 경우도 적어줘.형식에 변화는 주지말고 저 오행단어는 그대로 써줘 `;
+    const thirdResponse = await bard.question({ ask: thirdQuestion });
+    const cleanFirstResponse = firstResponse.content.replace(/\*/g, "");
+    const cleanSecondResponse = secondResponse.content.replace(/\*/g, "");
+    const cleanThirdResponse = thirdResponse.content.replace(/\*/g, "");
+
+    function formatFiveElements(response) {
+      const fiveElementsPattern =
+        /오행기운:\s*금\(金\):\s*(\d+)개\s*목\(木\):\s*(\d+)개\s*수\(水\):\s*(\d+)개\s*화\(火\):\s*(\d+)개\s*토\(土\):\s*(\d+)개/;
+      const match = response.match(fiveElementsPattern);
+      if (match) {
+        return `오행기운:\n금(金): ${match[1]}개\n목(木): ${match[2]}개\n수(水): ${match[3]}개\n화(火): ${match[4]}개\n토(土): ${match[5]}개`;
+      }
+      return "No match found";
     }
-  );
-  let response3 = await api.sendMessage(
-    "저 사주를 바탕으로 나와 어울리는 상대의 오행 갯수를 화(火): 0개 수(水): 0개 목(木): 0개  금(金): 0개 토(土): 0개 이런식으로 띄어쓰기와 설명없이 그냥 갯수만 말해줘.",
-    {
-      parentMessageId: response1.id,
-    }
-  );
 
-  return Response.json({
-    response1: response1.text,
-    response2: response2.text,
-    response3: response3.text,
-  });
+    let formattedResponse2 = formatFiveElements(cleanSecondResponse);
+    let formattedResponse3 = formatFiveElements(cleanThirdResponse);
+    console.log(cleanFirstResponse);
+    return Response.json({
+      response1: cleanFirstResponse,
+      response2: formattedResponse2,
+      response3: formattedResponse3,
+    });
+  } catch (error) {
+    return Response.json({ error: error.message });
+  }
 }
